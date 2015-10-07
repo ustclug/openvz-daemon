d33850f58db6aa5d11c302333669964e93a015a1
SJ Zhu <zsj950618@gmail.com>
2015-10-07 20:12:17 +0800

    add and modify api spec

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 * [GNU Libmicrohttpd](http://www.gnu.org/software/libmicrohttpd/)
 * [GnuTLS](http://www.gnutls.org/)
 * [json-c](https://github.com/json-c/json-c/wiki)
+
+## API
+
+### Overview
+
+URL           | GET           | POST          | PUT           | DELETE        |
+------------- | ------------- | ------------- | ------------- | ------------- |
+/vz           | list vz ids   |               |               |               |
+/vz/\<id\>    | get info      | create        | set parameter | delete        |
+/control/\<id\> |             | control a vz  |               |               |
+
+* control: start,stop,reboot,reinstall
--- url_handle.c
+++ url_handle.c
@@ -12,19 +12,29 @@ json_object *
 process_get(const char * url) {
     //printf("url:%s\n",url);
     /**
-     * GET /list
+     * GET /vz
      * return a json_list of all containers
      */
-    if(0 == strncmp(url, "/list", strlen("/list"))) {
-        json_object * data;
-        data = json_object_new_array();
-        run_command_block("vzlist -o ctid -a -H;", data, list_process);
-        return data;
+    if(0 == strncmp(url, "/vz", strlen("/vz"))) {
+        const char * url_end = url + strlen("/vz");
+        if('\0' == *url_end || ('/' == *url_end && '\0' == *(url_end+1))) {
+            json_object *data;
+            data = json_object_new_array();
+            run_command_block("vzlist -o ctid -a -H;", data, list_process);
+            return data;
+        }
     }
     /**
-     * /status/id
+     * GET /vz/id
+     * return the vz's info
      */
-    if(0 == strncmp(url, "/status", strlen("/status"))) {
+    if(0 == strncmp(url, "/vz/", strlen("/vz/"))) {
+        int id;
+        const char * url_id = url + strlen("/vz/");
+        if(NULL == url_id || *url_id < '0' || *url_id > '9')
+            return NULL;
+        sscanf(url + strlen("/vz/"),"%d", &id);
+        return NULL;
 
     }
     return NULL;
