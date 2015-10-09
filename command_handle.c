3c1fbb5629580363f2e8f70e02ac0c34e919a298
SJ Zhu <zsj950618@gmail.com>
2015-10-09 12:22:13 +0800

    add run_command_nonblock handle

--- command_handle.c
+++ command_handle.c
@@ -1,4 +1,5 @@
-#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <json.h>
 #define BUFFERSIZE 1024
@@ -17,4 +18,37 @@ int run_command_block(const char * command, json_object * data,
         }
     }
     return pclose(fp);
+}
+
+/**
+ * @param execute
+ * @param args normally args[0] is same to execute.
+ */
+int run_command_nonblock(const char * execute, char * const args[]) {
+    printf("We are in run_command_nonblock\n");
+    int status = 0;
+    int error = 0;
+
+    pid_t pid1 = fork();
+
+    if (pid1 > 0) {
+        /* parent process A */
+        waitpid(pid1, &status, NULL);
+    } else if (0 == pid1) {
+        /* child process B */
+        pid_t pid2 = fork();
+        printf("pid2: %d\n",pid2);
+        if (pid2 > 0) {
+            exit(0);
+        } else if (0 == pid2) {
+            /* child process C */
+            execvp(execute, args);
+        } else {
+            exit(1);
+        }
+    } else {
+        error = 1;
+    }
+    printf("pid1: %d\n",pid1);
+    return error || status;
 }
