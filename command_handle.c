6d5515685d845afa58423774cee13b6d0da65272
SJ Zhu <zsj950618@gmail.com>
2015-11-02 00:13:21 +0800

    command_handle: store nonblock-command output; fix z process

--- command_handle.c
+++ command_handle.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <json.h>
+#include <fcntl.h>
+
 #define BUFFERSIZE 1024
 int run_command_block(const char * command, json_object * data,
                       void (*cb)(const char * line, int line_no, json_object * cb_data)){
@@ -32,15 +34,26 @@ int run_command_nonblock(const char * execute, char * const args[]) {
 
     if (pid1 > 0) {
         /* parent process A */
-        waitpid(pid1, &status, WNOHANG);
+        waitpid(pid1, &status, 0);
     } else if (0 == pid1) {
         /* child process B */
         pid_t pid2 = fork();
         if (pid2 > 0) {
             exit(0);
         } else if (0 == pid2) {
+            pid_t pid3 = getpid();
+            char filename_stdout[BUFFERSIZE];
+            char filename_stderr[BUFFERSIZE];
+            snprintf(filename_stdout, BUFFERSIZE, "/tmp/openvz-daemon-%d.out", pid3);
+            snprintf(filename_stderr, BUFFERSIZE, "/tmp/openvz-daemon-%d.err", pid3);
+            int fd_stdout = open(filename_stdout, O_WRONLY|O_CREAT, S_IRUSR);
+            int fd_stderr = open(filename_stderr, O_WRONLY|O_CREAT, S_IRUSR);
+            dup2(fd_stdout, 1);
+            dup2(fd_stderr, 2);
             /* child process C */
             execvp(execute, args);
+            close(fd_stdout);
+            close(fd_stderr);
         } else {
             exit(1);
         }
